8eab8d18b8b6f8b138c3e12d1a3029cdf5672a86
poorniv-89 <poorniv.89@gmail.com>
2025-03-31 14:29:39 -0600

    Added game logic to the event handler

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const userResponseYes = document.getElementById('user-response-yes');
 const userResponseNo = document.getElementById('user-response-no');
 const mainEl = document.querySelector('div');
+const gameOptions = ['rock', 'paper', 'scissors'];
 console.log(mainEl);
 
 
@@ -16,7 +17,9 @@ function handleUserResponse(e)
     }
     else
     {
-        mainEl.forEach(el => el.style.visibility = 'hidden');
+        while (mainEl.firstChild) {
+            mainEl.firstChild.remove();
+        }
         let textEl = document.createElement('h1');
         textEl.textContent="Goodbye! Thanks for visiting the page!"
         document.body.appendChild(textEl);
@@ -54,10 +57,44 @@ function createUserName()
     }
 }
 function startGame(username) {
-    while(!username){
-            alert("Please enter your username before starting the game!");
-            return;
+        while (mainEl.firstChild) {
+            mainEl.firstChild.remove();
         }
-        console.log("Starting the game for user:"+ username);
-}
+        let fragment = document.createDocumentFragment();
+
+        gameOptions.forEach(option => {
+            let btn = document.createElement("button");
+            let img = document.createElement("img");
+    
+            img.src = `../images/${option}.jpg`;  
+            img.alt = option;
+            img.width = 150;           
+            btn.appendChild(img);
+            btn.addEventListener('click', playGame);
+            
+            fragment.appendChild(btn); 
+        });
+    
+        mainEl.appendChild(fragment); 
+    }
+        function playGame(e){
+            // e.preventDefault();
+            let userChoice = e.target.alt;
+            console.log(userChoice);
+            let computerChoice = gameOptions[Math.floor(Math.random()*3)];
+            console.log(computerChoice);
+           
+            if (userChoice === computerChoice) {
+                console.log("It's a tie!");
+            } else if (
+                (userChoice === "rock" && computerChoice === "scissors") ||
+                (userChoice === "paper" && computerChoice === "rock") ||
+                (userChoice === "scissors" && computerChoice === "paper")
+            ) {
+                console.log("You win!");
+            } else {
+                console.log("You lose!");
+
+            }
+        }
 
